12fe342fbd1ddf5f6b166be82023b2d4689f3e57
WisenextTime <z163zb@163.com>
2024-12-16 10:04:08 +0800

    Initial commit

---
+++ Scripts/Scene.cs
@@ -0,0 +1,115 @@
+using System.Linq;
+using Godot;
+
+namespace ThetaStar.Scripts;
+
+public partial class Scene : Control
+{
+	private int _mousePressed = -1;
+	private bool _launching;
+
+	private Vector2I LastMousePosition =>
+		new Vector2I((int)GetGlobalMousePosition().X / 32, (int)GetGlobalMousePosition().Y / 32);
+	private ItemList Selector => field ??= GetNode<ItemList>("Operation/Selector");
+	private TileMapLayer Map => field ??= GetNode<TileMapLayer>("Map");
+	private Line2D Path => field ??= GetNode<Line2D>("Path");
+
+	// ReSharper disable once FieldCanBeMadeReadOnly.Local
+	private int[,] _tiles = new int[30, 20];
+	private Vector2I _startPos = new(0, 19);
+	private Vector2I _endPos = new(29, 19);
+	
+	public override void _Ready()
+	{
+		Selector.Select(0);
+		for (var x = 0; x < 30; x++)
+		{
+			for (var y = 0; y < 20; y++)
+			{
+				_tiles[x, y] = 1;
+			}
+		}
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		if (_launching) return;
+		switch (@event)
+		{
+			case InputEventMouseButton { ButtonIndex: MouseButton.Left } mouseButton:
+				if (mouseButton.Pressed)
+				{
+					
+					var pos = LastMousePosition;
+					if (pos.X > 29 || pos.Y > 19)
+					{
+						_mousePressed = -1;
+						return;
+					}
+					if(Selector.GetSelectedItems()[0]==0)
+					{
+						_mousePressed = _tiles[pos.X, pos.Y];
+					}
+					else
+					{
+						SetCell(pos, 0);
+					}
+				}
+				else
+				{
+					_mousePressed = -1;
+				}
+				break;
+			case InputEventMouseMotion when _mousePressed != -1:
+				SetCell(LastMousePosition, _mousePressed);
+				break;
+		}
+	}
+
+	private void SetCell(Vector2I pos, int value)
+	{
+		if(pos.X > 29 || pos.Y > 19) return;
+		switch (Selector.GetSelectedItems()[0])
+		{
+			case 0:
+				if(pos == _startPos || pos == _endPos) break;
+				if (value == int.MaxValue)
+				{
+					Map.SetCell(pos, 0, new Vector2I(1, 0));
+					_tiles[pos.X, pos.Y] = 1;
+				}
+				else
+				{
+					Map.SetCell(pos, 0, new Vector2I(0, 0));
+					_tiles[pos.X, pos.Y] = int.MaxValue;
+				}
+				break;
+			case 1:
+				if (_tiles[pos.X, pos.Y] == 1) break;
+				Map.SetCell(_startPos, 0, new Vector2I(1, 0));
+				Map.SetCell(pos, 0, new Vector2I(2, 0));
+				_startPos = pos;
+				break;
+			case 2:
+				if (_tiles[pos.X, pos.Y] == 1) break;
+				Map.SetCell(_endPos, 0, new Vector2I(1, 0));
+				Map.SetCell(pos, 0, new Vector2I(3, 0));
+				_endPos = pos;
+				break;
+		}
+	}
+
+	public void Start()
+	{
+		_launching = true;
+		var final = ThetaStar.FindPath(_startPos, _endPos, _tiles);
+		if(final == null) return;
+		foreach (var node in final)
+		{
+			Path.AddPoint(node * 32 + new Vector2I(16, 16));
+			if(node == _endPos) continue;
+			Map.SetCell(node, 0, new Vector2I(0, 1));
+		}
+		Path.AddPoint(_startPos * 32 + new Vector2I(16, 16));
+	}
+}
---
+++ Scripts/ThetaStar.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
+
+namespace ThetaStar.Scripts;
+
+public class ThetaStar
+{
+	public static List<Vector2I> FindPath(Vector2I from, Vector2I to, int[,] map)
+	{
+		List<Node> open = [];
+		List<Node> closed = [];
+		List<Node> Walls = [];
+		var grid = new Dictionary<Vector2I, Node>();
+		for (var x = 0; x < map.GetLength(0); x++)
+		{
+			for (var y = 0; y < map.GetLength(1); y++)
+			{
+				var newNode = new Node(new Vector2I(x, y), from, to,
+					new Vector2I(map.GetLength(0), map.GetLength(1)),
+					map[x, y]);
+				grid.Add(new Vector2I(x, y), newNode);
+				if (newNode.Cost == int.MaxValue) Walls.Add(newNode);
+			}
+		}
+		var startNode = grid[from];
+		var endNode = grid[to];
+		open.Add(startNode);
+		while (open.Any())
+		{
+			var current = open[0];
+			foreach (var node in open.Where(node =>
+				         node.FValue < current.FValue ||
+				         node.FValue.Equals(current.FValue) && node.HValue < current.HValue))
+				current = node;
+			closed.Add(current);
+			open.Remove(current);
+
+			if (current == endNode)
+			{
+				var currentNode = endNode;
+				var path = new List<Vector2I>();
+				while (currentNode != startNode)
+				{
+					path.Add(currentNode.Position);
+					currentNode = currentNode.Parent;
+				}
+				return path;
+			}
+			
+			foreach (var neighbor in current.GetNeighbors(grid).Where(n => !closed.Contains(n)))
+			{
+				var inSearch = open.Contains(neighbor);
+				var cost = current.GValue + current.DistanceTo(neighbor);
+				if (!inSearch || cost <= neighbor.GValue)
+				{
+					neighbor.GValue = cost;
+					neighbor.Parent = current;
+					
+					if (!inSearch)
+					{
+						open.Add(neighbor);
+					}
+				}
+			}
+		}
+		return null;
+	}
+}
+
+// ReSharper disable NotAccessedPositionalProperty.Global
+public record Node(Vector2I Position, Vector2I Start, Vector2I End, Vector2I Size, int Cost)
+{
+	public Vector2I Position = Position;
+	public Vector2I Size = Size;
+	public float HValue = Position.DistanceTo(Start);
+	public float GValue = Position.DistanceTo(End);
+
+	public float UpperBound = float.PositiveInfinity;
+	public float LowerBound = float.NegativeInfinity;
+	
+	public int Cost = Cost;
+	public Node Parent;
+	public float FValue => GValue + HValue + Cost;
+	public float DistanceTo(Node target) => Position.DistanceTo(target.Position);
+
+	private readonly bool _onLeft = Position.X == 0;
+	private readonly bool _onRight = Position.X == Size.X - 1;
+	private readonly bool _onTop = Position.Y == 0;
+	private readonly bool _onBottom = Position.Y == Size.Y - 1;
+
+	public List<Node> GetNeighbors(Dictionary<Vector2I, Node> map)
+	{
+		List<Vector2I> neighbors = [];
+		if (!_onLeft) neighbors.Add(new Vector2I(Position.X - 1, Position.Y));
+		if (!_onTop) neighbors.Add(new Vector2I(Position.X, Position.Y - 1));
+		if (!_onRight) neighbors.Add(new Vector2I(Position.X + 1, Position.Y));
+		if (!_onBottom) neighbors.Add(new Vector2I(Position.X, Position.Y + 1));
+
+		if (!_onLeft && !_onTop) neighbors.Add(new Vector2I(Position.X - 1, Position.Y - 1));
+		if (!_onRight && !_onTop) neighbors.Add(new Vector2I(Position.X + 1, Position.Y - 1));
+		if (!_onLeft && !_onBottom) neighbors.Add(new Vector2I(Position.X - 1, Position.Y + 1));
+		if (!_onRight && !_onBottom) neighbors.Add(new Vector2I(Position.X + 1, Position.Y + 1));
+		
+		return neighbors.Select(node => map[node]).ToList();
+	}
+}
